fe4a24c975de81a2f1611b0b985b889afbeda068
lixm1988 <527725681@qq.com>
2020-04-24 17:37:19 +0800

    添加共享桌面的target

---
+++ VideoCallDemo/EMiOSVideoCallDemo/SharedDesktop/SampleHandler.h
@@ -0,0 +1,13 @@
+//
+//  SampleHandler.h
+//  SharedDesktop
+//
+//  Created by lixiaoming on 2020/4/24.
+//  Copyright © 2020 easemob. All rights reserved.
+//
+
+#import <ReplayKit/ReplayKit.h>
+
+@interface SampleHandler : RPBroadcastSampleHandler
+
+@end
